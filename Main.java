922d68f93c73f4a90a03c9b19f840d54aefdac11
Ben Krokower <benkrok@gmail.com>
2025-10-21 07:47:11 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("What is your name, bro? ");
+        System.out.print("What is your name? ");
         String name = scanner.nextLine();
         
         System.out.println("Your name is " + name);
